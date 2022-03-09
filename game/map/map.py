0274cfc1414ccf8c357390eaad4b701871b29802
Shiyukine <benjbres@gmail.com>
2022-03-09 18:06:49 +0100

    map finie. tests en attente

--- game/map/map.py
+++ game/map/map.py
@@ -5,10 +5,14 @@ from display.entity.sheep import BLACK_SHEEP, WHITE_SHEEP, Sheep
 
 class Map:
 
-    def __init__(self, console_width, console_height) -> None:
-        self.__actual_frame = []
-        self.__pos_next_objects = {}
-        self.__cur_pos = (0, 0)
+    def __init__(self) -> None:
+        """Crée la map automatiquement petit à petit.
+        Après avoir appelé __init__, changez max_pos et min_pos si vous voulez puis appelez create_map pour initialiser la map."""
+        # liste des entités actuels sur la map
+        self.__pos_next_entities = {}
+        # 1 pixel sur la console = 1 pos(ition)
+        self.__cur_pos = 0
+        # TO-DO: ajouter d'autres entités
         self.__entities = [
             "white_sheep"
             "black_sheep",
@@ -18,30 +22,29 @@ class Map:
         self.max_pos = 100
         # idem mais le mini
         self.min_pos = 10
-        self.console_width = console_width
-        self.console_height = console_height
 
     def next_frame(self):
-        self.__actual_frame = []
-        if len(self.__pos_next_objects) == 0:
-            self.__add_new_object()
+        """Actualise la map en avançant les entités sur la map"""
+        if len(self.__pos_next_entities) == 0:
+            self.__add_new_entity()
         else:
-            last_pos = self.__pos_next_objects.keys()[-1]
-            last_entity = self.__pos_next_objects[last_pos]
+            last_pos = self.__pos_next_entities.keys()[-1]
+            #last_entity = self.__pos_next_entities[last_pos]
             if last_pos[0] - self.__cur_pos < self.max_pos:
-                self.__add_new_object()
+                self.__add_new_entity()
             if last_pos[0] < self.__cur_pos:
-                self.__pos_next_objects.pop(0)
-        # for entity in self.__pos_next_objects.items():
-        #    entity.x
+                self.__pos_next_entities.pop(0)
+        for i in self.__pos_next_entities.items():
+            self.__pos_next_entities[i].x = i[0] - self.__cur_pos
         self.__cur_pos += 1
-        return self.__actual_frame
+        return self.__pos_next_entities
 
     def create_map(self):
+        """Initialise la map"""
         self.next_frame()
         pass
 
-    def __add_new_object(self):
+    def __add_new_entity(self):
         obj_pos = self.__cur_pos + random.randint(self.min_pos, self.max_pos)
         obj_name = random.choice(self.__entities)
         obj = None
@@ -51,4 +54,4 @@ class Map:
             obj = Sheep(BLACK_SHEEP)
         else:
             obj = Entity()
-        self.__pos_next_objects[(obj_pos, obj.y)] = obj
+        self.__pos_next_entities[(obj_pos, obj.y)] = obj
