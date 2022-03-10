2133304e2cdd41018c811779de38217211badf5b
Guillaume Seimandi <36552311+iSeaox@users.noreply.github.com>
2022-03-10 08:52:34 +0100

    Update console.py

--- display/console.py
+++ display/console.py
@@ -1,5 +1,5 @@
 class Console:
-
+    """Cette classe permet de gérer la console et l'affichage sur celle-ci"""
     def __init__(self, height, width):
         self.width = width
         self.height = height
@@ -7,6 +7,12 @@ class Console:
 
 
     def blit(self, textures, x, y):
+        """Dessine sur le canvas de la console la textures donnée en paramètre 
+        aux coordonnées (x, y). Il est a noté que le (0, 0) se situe tout en haut à gauche 
+        De plus, si la texture est partiellement ou entièrement en dehors des coordonnées
+        admis, (de (0, 0) à (self.width - 1, self.height - 1)), la méthode ajoutera au 
+        canvas uniquement les parties visibles"""
+
         t_splited = textures.split("\n")
         for i in range(len(t_splited)):
             j = 0
@@ -20,6 +26,7 @@ class Console:
         return self.__canvas
 
     def clear_canvas(self):
+        """Renvoie un nouveau canvas vide"""
         self.__canvas = []
         for line in range(self.height):
             temp = []
@@ -28,6 +35,8 @@ class Console:
             self.__canvas.append(temp)
 
     def render(self):
+        """Renvoie une liste de string qui correspond aux lignes à 
+        afficher dans la console python""" 
         temp = []
         for i in range(self.height):
             line = ""
