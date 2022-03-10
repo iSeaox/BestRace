559ca023e9e5176f50de46a93f0d8231b9108c34
Guillaume Seimandi <36552311+iSeaox@users.noreply.github.com>
2022-03-10 08:36:10 +0100

    Update sheep.py

--- display/entity/sheep.py
+++ display/entity/sheep.py
@@ -7,7 +7,9 @@ WHITE_SHEEP = False
 
 class Sheep(entity.Entity):
     def __init__(self, sheep_color):
-        super().__init__()
+        super().__init__() # permet d'appeler le constructeur de la superclass 
+        # ici entity.Entity
+
         self.__color = sheep_color
         if(self.__color == BLACK_SHEEP):
             self.set_sprite_sheet(t_sheep.black_sheep_animation)
@@ -16,7 +18,11 @@ class Sheep(entity.Entity):
 
 
     def render(self, console, game):
+        """Permet de faire le rendu du mouton et de l'inclure dans l'object console
+        avant l'affichage """
         console.blit(self.get_sprite(), self.x, game.floor_height - 24)
 
     def get_color(self):
+        """Renvoie la color du monton. Les valeurs retournées
+        dépende des variables BLACK_SHEEP et WHITE_SHEEP"""
         return self.__color
