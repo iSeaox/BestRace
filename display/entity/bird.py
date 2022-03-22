ef062b9909d9318f039abeb73b1057262b4933c0
TagCopperLight <tagcopperlight@gmail.com>
2022-03-22 15:03:13 +0100

    Add files via upload

--- display/entity/bird.py
+++ display/entity/bird.py
@@ -4,10 +4,13 @@ import textures.bird as t_bird
 
 
 class Bird(entity.Entity):
+    """Cette classe permet de gérer les oiseaux"""
     def __init__(self):
         super().__init__()
         self.set_sprite_sheet(t_bird.bird_animation)
         self.y = 50
 
     def render(self, console, game):
+        """Permet de faire le rendu de l'oiseau et de l'inclure dans l'object console
+        avant l'affichage """
         console.blit(self.get_sprite(), self.x, self.y)
