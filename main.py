d6355ed0377f20a6a63e6667091254c48257fd92
Shiyukine <benjbres@gmail.com>
2022-04-03 19:40:32 +0200

    ajout de la documentation concernant main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import utils.storage_handler as s_handler
 import os
 import sys
 import colorama
-
 import ctypes
 
+# Modifier la taille de la police d'écriture
 LF_FACESIZE = 32
 STD_OUTPUT_HANDLE = -11
 
@@ -29,16 +29,13 @@ class CONSOLE_FONT_INFOEX(ctypes.Structure):
 
 font = CONSOLE_FONT_INFOEX()
 font.cbSize = ctypes.sizeof(CONSOLE_FONT_INFOEX)
-#font.nFont = 9
-#font.dwFontSize.X = 5
 font.dwFontSize.Y = 5
-#font.FontFamily = 9
-#font.FontWeight = 9
 font.FaceName = "Lucida Console"
 
 handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
 ctypes.windll.kernel32.SetCurrentConsoleFontEx(
     handle, ctypes.c_long(False), ctypes.pointer(font))
+#
 
 sys.path.insert(1, os.path.abspath("."))  # Permet d'avoir des imports
 # plus clair lorsqu'il y a plusieurs packages ce qui est le cas ici
@@ -48,8 +45,10 @@ sys.path.insert(1, os.path.abspath("."))  # Permet d'avoir des imports
 
 colorama.init()
 
+# Modifier la taille de la fenêtre
 cmd = 'mode 400,101'
 os.system(cmd)
+#
 
 game_obj = game.Game()
 key_handler.enable(game_obj)
